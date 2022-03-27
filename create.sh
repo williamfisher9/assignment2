03fcdb2321695c39d75cb9d2e72d8f560b441cac
williamfisher9 <98424965+williamfisher9@users.noreply.github.com>
2022-03-27 08:37:48 -0500

    Update create.sh

--- create.sh
+++ create.sh
@@ -5,9 +5,7 @@ aws cloudformation create-stack \
 --parameters file://$3
 
 
-
-#aws cloudformation create-stack --stack-name stack-network --template-body file://1_network.yml --parameters file://1_network_params.json
-
-#aws cloudformation create-stack --stack-name stack-apps --template-body file://2_servers.yml --parameters file://2_servers_params.json
-
-#aws cloudformation create-stack --stack-name stack-db --template-body file://3_option_2_ec2_mysql.yml --parameters file://3_option_2_ec2_mysql_params.json
+# or you can run the below three commands:
+# aws cloudformation create-stack --stack-name stack-network --template-body file://1_network.yml --parameters file://1_network_params.json
+# aws cloudformation create-stack --stack-name stack-apps --template-body file://2_servers.yml --parameters file://2_servers_params.json
+# aws cloudformation create-stack --stack-name stack-db --template-body file://3_option_2_ec2_mysql.yml --parameters file://3_option_2_ec2_mysql_params.json
